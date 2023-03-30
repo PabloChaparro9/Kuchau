3cf238d354f4f9a3cd135ad2b32a4d3dede0c08a
Pablo Chaparro <pablo.47.114@gmail.com>
2023-03-29 23:42:11 -0300

    footer y buscador

--- js/script.js
+++ js/script.js
@@ -6,9 +6,25 @@ fetch("./database.json")
         for(const producto of data){
             contenidoDestacadoHTML.innerHTML += `
             <div class="card">
-            <img src='${producto.url}'>
+            <img src='${producto.img}'>
+            <p>modelo de auto: ${producto.name.toLowerCase()}</p>
+            <p>peso: ${producto.price}</p>
+            </div>
+            `
+        }
+    })
+    
+const contenidoProductosHTML = document.getElementById("contenidoProductos")
+
+fetch("../database.json")
+    .then(response => response.json())
+    .then(data =>{
+        for(const producto of data){
+            contenidoProductosHTML.innerHTML += `
+            <div class="card">
+            <img src='${producto.imgpages}'>
             <p>modelo de auto: ${producto.name}</p>
-            <p>año: ${producto.year}</p>
+            <p>peso: ${producto.peso}</p>
             </div>
             `
         }
